7130f76603af4ec8bdf458ea9bb045243aaf5a9b
Sam Frons <samfrons@gmail.com>
2023-09-22 14:28:56 +0200

    Initial commit

---
+++ components/antdesignprovider/antdesignprovider.tsx
@@ -0,0 +1,8 @@
+import React, { FC, PropsWithChildren } from 'react'
+import { ConfigProvider } from 'antd'
+
+const Provider: FC<PropsWithChildren<any>> = ({ children }) => {
+  return <ConfigProvider theme={undefined}>{children}</ConfigProvider>
+}
+
+export default Provider
